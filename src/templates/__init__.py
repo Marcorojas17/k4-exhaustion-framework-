fa19c540f7ebc12037c0e9aa138d08d2e1d5924b
Marcorojas17 <86907529+Marcorojas17@users.noreply.github.com>
2026-09-06 22:25:25 -0600

    Initialize templates module with engines

---
+++ src/templates/__init__.py
@@ -0,0 +1,8 @@
+# src/templates/__init__.py
+from .inversion import InversionTemplateEngine
+from .hill import HillMatrixEngine
+
+__all__ = [
+    "InversionTemplateEngine",
+    "HillMatrixEngine"
+]
